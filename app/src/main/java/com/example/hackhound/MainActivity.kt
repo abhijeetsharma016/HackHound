64dbfa973f7f9502b26bf6c05dfb0528f75395ae
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2025-01-22 22:23:07 +0530

    starting of project

--- app/src/main/java/com/example/hackhound/MainActivity.kt
+++ app/src/main/java/com/example/hackhound/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var NavController = findNavController(R.id.fragmentContainerView2)
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener{
+            val bottomSheetDialog = notification_bottom_fragment()
+            bottomSheetDialog.show(supportFragmentManager, "test")
         }
+
     }
 }
